1a8ac37c071fd308cbe9a238f519604c7d016b66
Bruno Cavaletti Alves <bcalves@apoioprodesp.gov.sp.br>
2023-11-21 15:43:32 -0300

    chore: project setup

---
+++ Xcta/Sources/XctaApp.swift
@@ -0,0 +1,17 @@
+//
+//  XctaApp.swift
+//  Xcta
+//
+//  Created by Bruno Alves on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct XctaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello")
+        }
+    }
+}
